894dc3147c37110a1ec625750dfa891ee0153ad4
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-06-04 13:30:21 -0400

    serve wiki pages

--- wiki.go
+++ wiki.go
@@ -2,7 +2,9 @@ package main
 
 import (
     "fmt"
+	"log"
     "os"
+	"net/http"
 )
 
 type Page struct {
@@ -24,9 +26,17 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+    http.HandleFunc("/view/", viewHandler)
+    log.Fatal(http.ListenAndServe(":8081", nil))
 }
